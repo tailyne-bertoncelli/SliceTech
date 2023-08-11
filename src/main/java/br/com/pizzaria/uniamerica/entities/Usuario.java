599613d627ab056003412c637ca543546e49f99c
unknown <cristovaom22@gmail.com>
2023-08-11 18:02:34 -0300

    feat(Usuario #3): Criando a entity de Usuario

---
+++ src/main/java/br/com/pizzaria/uniamerica/entities/Usuario.java
@@ -0,0 +1,34 @@
+package br.com.pizzaria.uniamerica.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_usuarios",schema = "public")
+@NoArgsConstructor
+public class Usuario extends AbstractEntity{
+    @Getter
+    @Setter
+    @Column(name = "login")
+    private String login;
+    @Getter
+    @Setter
+    @Column(name = "senha")
+    private String senha;
+    @Getter
+    @Setter
+    @Column(name = "email")
+    private String email;
+    @Enumerated(EnumType.STRING)
+    private Cargo cargo;
+
+    public Usuario(String login, String senha, String email, Cargo cargo) {
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+        this.cargo = cargo;
+    }
+}
