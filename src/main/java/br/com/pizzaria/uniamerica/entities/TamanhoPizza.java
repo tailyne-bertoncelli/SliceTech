94f773337efba67b303bc50e41b6466c995349c8
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-08-13 16:51:23 -0300

    feat(Pizza - #7): Criando entidade de pizza

---
+++ src/main/java/br/com/pizzaria/uniamerica/entities/TamanhoPizza.java
@@ -0,0 +1,11 @@
+package br.com.pizzaria.uniamerica.entities;
+
+public enum TamanhoPizza {
+    PEQUENA("Pequena"),
+    MÉDIA("Média"),
+    GRANDE("Grande"),
+    GIGANTE("Gigante");
+
+    TamanhoPizza(String tamanho) {
+    }
+}
