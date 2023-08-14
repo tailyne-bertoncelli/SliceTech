ee8683d1ddd66c016faa81a9cc4ce942edad25e9
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-08-13 23:18:50 -0300

    feat(EnderecoDTO - #17): Criando o DTO de cadastrar Endereco

---
+++ src/main/java/br/com/pizzaria/uniamerica/dto/enderecoDTOs/DadosCadastraEndereco.java
@@ -0,0 +1,8 @@
+package br.com.pizzaria.uniamerica.dto.enderecoDTOs;
+
+public class DadosCadastraEndereco {
+    private String logradouro;
+    private int numero;
+    private String cep;
+    private String complemento;
+}
