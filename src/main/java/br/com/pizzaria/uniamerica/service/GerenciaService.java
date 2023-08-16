30b6a84227b664da08b06afe1a3c7b3ee0e4fa45
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-08-16 18:42:43 -0300

    feat(GerenciaService - #29): Criando funções que buscam os relatórios

--- src/main/java/br/com/pizzaria/uniamerica/service/GerenciaService.java
+++ src/main/java/br/com/pizzaria/uniamerica/service/GerenciaService.java
@@ -10,5 +10,36 @@ import java.util.List;
 
 @Service
 public class GerenciaService {
+    @Autowired
+    private GerenciaService gerenciaService;
 
+    public List<Pedido> pedidosDoDia(LocalDate date){
+        List<Pedido> pedidoList = this.gerenciaService.pedidosDoDia(date);
+        return pedidoList;
+    }
+
+    public List<Pedido> pedidosEncerrados(LocalDate date){
+        List<Pedido> pedidoList = this.gerenciaService.pedidosEncerrados(date);
+        return pedidoList;
+    }
+
+    public List<Pedido> pedidosEntrega(LocalDate date){
+        List<Pedido> pedidoList = this.gerenciaService.pedidosEntrega(date);
+        return pedidoList;
+    }
+
+    public List<Pedido> pedidosRetira(LocalDate date){
+        List<Pedido> pedidoList = this.gerenciaService.pedidosRetira(date);
+        return pedidoList;
+    }
+
+    public Long totalPedidosDinheiro(LocalDate date){
+        Long total = this.gerenciaService.totalPedidosDinheiro(date);
+        return total;
+    }
+
+    public Long totalPedidosCartao(LocalDate date){
+        Long total = this.gerenciaService.totalPedidosCartao(date);
+        return total;
+    }
 }
