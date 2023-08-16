7df89c6868653d357dfbd6c10a5f3fd321d7a66d
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-08-16 18:40:08 -0300

    feat(GerenciaRepository - #36): Criando as querrys para busca dos relatórios

--- src/main/java/br/com/pizzaria/uniamerica/repository/GerenciaRepository.java
+++ src/main/java/br/com/pizzaria/uniamerica/repository/GerenciaRepository.java
@@ -1,9 +1,33 @@
 package br.com.pizzaria.uniamerica.repository;
 
 import br.com.pizzaria.uniamerica.entities.Gerecia;
+import br.com.pizzaria.uniamerica.entities.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface GerenciaRepository extends JpaRepository<Gerecia, Long> {
+    @Query("SELECT pedido FROM Pedido pedido WHERE pedido.cadastro = :data")
+    List<Pedido> totalPedidos(@Param("data") LocalDate date);
+
+    @Query("SELECT pedido FROM Pedido pedido WHERE pedido.cadastro = :data AND pedido.situacao = true")
+    List<Pedido> totalPedidosEncerrados(@Param("data") LocalDate date);
+
+    @Query("SELECT pedido FROM Pedido pedido WHERE pedido.cadastro = :data AND pedido.entrega = true")
+    List<Pedido> totalPedidosEntrega(@Param("data") LocalDate date);
+
+    @Query("SELECT pedido FROM Pedido pedido WHERE pedido.cadastro = :data AND pedido.entrega = false")
+    List<Pedido> totalPedidosRetira(@Param("data") LocalDate date);
+
+    @Query("SELECT sum(pedido.valor) FROM Pedido pedido WHERE pedido.cadastro = :data")
+    Long valorTotalVendasCartao(@Param("data") LocalDate date);
+
+    @Query("SELECT sum(pedido.valor) FROM Pedido pedido WHERE pedido.cadastro = :data")
+    Long valorTotalVendasDinheiro(@Param("data") LocalDate date);
+
 }
---
+++ src/main/java/br/com/pizzaria/uniamerica/service/GerenciaService.java
@@ -0,0 +1,14 @@
+package br.com.pizzaria.uniamerica.service;
+
+import br.com.pizzaria.uniamerica.entities.Pedido;
+import br.com.pizzaria.uniamerica.entities.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class GerenciaService {
+
+}
