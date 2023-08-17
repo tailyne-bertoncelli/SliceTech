7135b6a26ae4bc8ac6e7300b2a67f9e6df995f2d
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-08-17 20:02:06 -0300

    feat(Pizza - #46): Implementando validações em pizza

--- src/main/java/br/com/pizzaria/uniamerica/dto/pizzaDTOs/PizzaDTO.java
+++ src/main/java/br/com/pizzaria/uniamerica/dto/pizzaDTOs/PizzaDTO.java
@@ -2,6 +2,11 @@ package br.com.pizzaria.uniamerica.dto.pizzaDTOs;
 
 import br.com.pizzaria.uniamerica.entities.Pizza;
 import br.com.pizzaria.uniamerica.entities.TamanhoPizza;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,20 +16,17 @@ import java.util.Optional;
 @NoArgsConstructor
 public class PizzaDTO {
     @Getter @Setter
+    @NotBlank(message = "O sabor não pode ser null ou vazio!")
     private String sabor;
     @Getter @Setter
     private String descricao;
     @Getter @Setter
+    @DecimalMin(value = "0.01", message = "O valor não pode ser 0 ou null!")
     private double valor;
     @Getter @Setter
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O tamanho da pizza não pode ser null ou vazio!")
     private TamanhoPizza tamanhoPizza;
-
-    public PizzaDTO(Optional<Pizza> pizza) {
-        this.sabor = pizza.get().getSabor();
-        this.descricao = pizza.get().getDescricao();
-        this.valor = pizza.get().getValor();
-        this.tamanhoPizza = pizza.get().getTamanhoPizza();
-    }
 
     public PizzaDTO(Pizza pizza1) {
         this.sabor = pizza1.getSabor();
