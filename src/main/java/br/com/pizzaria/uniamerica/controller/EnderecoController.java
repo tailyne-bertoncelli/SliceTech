1129b407b71e83138e95e4c116c8f9e6be613dcc
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-08-16 21:50:51 -0300

    feat(Endereco - #37): Criando controller de Endereco

---
+++ src/main/java/br/com/pizzaria/uniamerica/controller/EnderecoController.java
@@ -0,0 +1,62 @@
+package br.com.pizzaria.uniamerica.controller;
+
+import br.com.pizzaria.uniamerica.dto.enderecoDTOs.EnderecoDTO;
+import br.com.pizzaria.uniamerica.entities.Endereco;
+import br.com.pizzaria.uniamerica.service.EnderecoService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoController {
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @GetMapping
+    public ResponseEntity<?> buscaPeloId(@RequestBody Long id){
+        try {
+            return ResponseEntity.ok(this.enderecoService.findById(id));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/lista-todos")
+    public ResponseEntity<?> buscaTodos(){
+        try {
+            return ResponseEntity.ok(this.enderecoService.findAll());
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping @Transactional
+    public ResponseEntity<?> cadastra(@RequestBody EnderecoDTO enderecoDTO){
+        try {
+            return ResponseEntity.ok(this.enderecoService.cadastra(enderecoDTO));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping @Transactional
+    public ResponseEntity<?> altera(@RequestBody Endereco endereco){
+        try {
+            return ResponseEntity.ok(this.enderecoService.altera(endereco));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping @Transactional
+    public ResponseEntity<?> desativa(@RequestBody Long id){
+        try {
+            this.enderecoService.desativa(id);
+            return ResponseEntity.ok("Endereço desativado com sucesso!");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
--- src/main/java/br/com/pizzaria/uniamerica/entities/Gerecia.java
+++ src/main/java/br/com/pizzaria/uniamerica/entities/Gerecia.java
@@ -8,8 +8,8 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Table
-@Entity(name = "tb_gerencia")
+@Table(name = "tb_gerencia", schema = "public")
+@Entity
 public class Gerecia extends AbstractEntity{
     @Getter @Setter
     @Column(name = "relatoriosGerados")
